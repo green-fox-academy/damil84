80675141c01885e8fb252021fcff2463e98f96fc
damil84 <damil84@gmail.com>
2017-11-28 16:53:09 +0100

    week-02/day-2 exercise ThirdArray is ready, exercise CompareLenngth folder added

--- week-02/day-2/Exercise_Third_Array/Program.cs
+++ week-02/day-2/Exercise_Third_Array/Program.cs
@@ -3,10 +3,17 @@
 namespace Exercise_Third_Array
 {
     class Program
+
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] q = new int [5];
+            q [0] = 4;
+            q [1] = 5;
+            q [2] = 6;
+            q [3] = 7;
+            Console.WriteLine("The third element is: " + q[2]);
+            Console.ReadKey();
         }
     }
 }
