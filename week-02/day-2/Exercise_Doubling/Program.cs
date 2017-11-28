6e09493cf91b453f8e99da295a9a64f3a0b78f20
damil84 <damil84@gmail.com>
2017-11-28 11:25:27 +0100

    week-02/day-2 exercise double added

---
+++ week-02/day-2/Exercise_Doubling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
