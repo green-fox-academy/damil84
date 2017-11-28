c31ca2bf73b90a02263a1e9d71bb199d1787122a
damil84 <damil84@gmail.com>
2017-11-28 14:37:15 +0100

    week-02/day-2 exercise Factorio is ready, exercise Printer's folder added

--- week-02/day-2/Exercise_Factorio/Program.cs
+++ week-02/day-2/Exercise_Factorio/Program.cs
@@ -6,7 +6,21 @@ namespace Exercise_Factorio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please give me the number of your choosing: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Factorio(number);
+            Console.ReadLine();
+
+
+        }
+        static void Factorio(int number)
+        {
+            int f = number;
+            for (int i = 1; i< number; i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine("The factorial of your given number is: " + f );
         }
     }
 }
