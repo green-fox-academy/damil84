e82a39548ca2e45ef419022873ccd2ff78e9903f
damil84 <damil84@gmail.com>
2017-11-29 12:36:00 +0100

    week-02/day-2 exercise ChangeElement is ready, and exercise IncremenentElement folder added

---
+++ week-02/day-2/Exercise_ChangeElement/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise_ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+
+            Console.WriteLine("Original fourth element is: " + s[3]);
+
+            s[3] = 4;
+            Console.WriteLine("The replaced fourth element is now on: " + s[3]);
+            Console.Read();
+        }
+    }
+}
---
+++ week-02/day-2/Exercise_IncrementElement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
