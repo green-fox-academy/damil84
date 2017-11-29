c035c047feeed62f257d4f1791afeb0b682acc94
damil84 <damil84@gmail.com>
2017-11-29 14:07:48 +0100

    week-02/day-2 exercise PrintElements is ready, and exercise DiagonalMatrix's folder added

--- week-02/day-2/Exercise_PrintElements/Program.cs
+++ week-02/day-2/Exercise_PrintElements/Program.cs
@@ -6,7 +6,10 @@ namespace Exercise_PrintElements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] af = { 4, 5, 6, 7 };
+            Console.WriteLine("This array has the following elements: {0}", string.Join(", ", af));
+            Console.ReadLine();
+          
         }
     }
 }
