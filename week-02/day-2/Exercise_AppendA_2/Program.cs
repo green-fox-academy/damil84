20f5b9fdf75e052b73e506c2dd01fb4719265680
damil84 <damil84@gmail.com>
2017-11-29 16:51:17 +0100

    week-02/day-2 exercise DiagonalMatrix is readyColors, and exercise AppendA_2's folder has added

---
+++ week-02/day-2/Exercise_AppendA_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_AppendA_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- week-02/day-2/Exercise_Colors/Program.cs
+++ week-02/day-2/Exercise_Colors/Program.cs
@@ -6,7 +6,24 @@ namespace Exercise_Colors
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[][] colors = new string[3][];
+            colors[0] = new string[5] { "lime", "forest green", "olive", "pale green", "spring green" };
+            colors[1] = new string[3] { "orange red", "red", "tomato" };
+            colors[2] = new string[4] { "orchid", "violet", "pink", "hot pink" };
+            Print(colors);
+            Console.ReadLine();
+        }
+
+        private static void Print(string[][] array)
+        {
+            foreach (string[] array2 in array)
+            {
+                foreach (string element in array2)
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("***************************");
+            }
         }
     }
 }
