33a31b65a36a81301913158187598f9bfdae1beb
damil84 <damil84@gmail.com>
2017-11-29 20:29:44 +0100

    week-02/day-2 exercise Reverse is ready

--- week-02/day-2/Exercise_Reverse/Program.cs
+++ week-02/day-2/Exercise_Reverse/Program.cs
@@ -6,7 +6,15 @@ namespace Exercise_Reverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] aj = { 3, 4, 5, 6, 7 };
+            for (int i = 0; i < aj.Length / 2; i++)
+            {
+                int tmp = aj[i];
+                aj[i] = aj[aj.Length - i - 1];
+                aj[aj.Length - i - 1] = tmp;
+            }
+            Console.WriteLine("Reverse order: {0}", string.Join(", ", aj));
+            Console.ReadLine();
         }
     }
 }
