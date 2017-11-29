98dcf28f218d9b68a60add856eab4d95c1902e52
damil84 <damil84@gmail.com>
2017-11-29 17:35:45 +0100

    week-02/day-2 exercise SwapElements is readyColors, and exercise SumAll's folder has added

---
+++ week-02/day-2/Exercise_SumAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- week-02/day-2/Exercise_SwapElements/Program.cs
+++ week-02/day-2/Exercise_SwapElements/Program.cs
@@ -6,7 +6,18 @@ namespace Exercise_SwapElements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] abc = { "first", "second", "third"};
+            string tmp = abc[0];
+            abc[0] = abc[2];
+            abc[2] = tmp;
+
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+            Console.WriteLine(abc[i]);
+            }
+            Console.ReadLine();
         }
+
     }
 }
