5b905fcd02752379a4165a01be6d1cf81f561501
damil84 <damil84@gmail.com>
2017-11-29 15:41:29 +0100

    week-02/day-2 exercise DiagonalMatrix is readyDoubleItems, and exercise Colors' folder has added

--- week-02/day-2/Exercise_DoubleItems/Program.cs
+++ week-02/day-2/Exercise_DoubleItems/Program.cs
@@ -6,7 +6,17 @@ namespace Exercise_DoubleItems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] ag = { 3, 4, 5, 6, 7 };
+
+            for (int i = 0; i < ag.Length; i++)
+            {
+                Console.WriteLine("Original element: {0} ", ag[i]);
+                ag[i] *= 2;
+                Console.WriteLine("Doubled value of element: {0}", ag[i]);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
+
     }
 }
