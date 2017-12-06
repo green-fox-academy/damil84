c79c67f1a1670350a60c91f978698688343a8092
damil84 <damil84@gmail.com>
2017-12-06 14:05:06 +0100

    week-03/day-3 Exercise_ColorBox and Exercise3_Diagonals are ready

---
+++ week-03/day-3/Exercise2_ColorBox/Exercise2_ColorBox/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GreenFox;
+
+namespace Exercise2_ColorBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+            DrawGreenLines(foxDraw);
+        }
+
+        public static void DrawGreenLines(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(50, 50, 550, 50);
+
+            foxDraw.StrokeColor(Colors.Blue);
+            foxDraw.DrawLine(50, 550, 550, 550);
+
+            foxDraw.StrokeColor(Colors.Yellow);
+            foxDraw.DrawLine(50, 50, 50, 550);
+
+            foxDraw.StrokeColor(Colors.Black);
+            foxDraw.DrawLine(550, 50, 550, 550);
+
+        }
+    }
+}
