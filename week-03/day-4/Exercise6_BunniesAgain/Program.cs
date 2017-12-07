55a2467bfb9e0026956224bcb3863116df620846
damil84 <damil84@gmail.com>
2017-12-07 15:38:12 +0100

    (fear)Exercise6_BunniesAgain From user input we could count how many total bunny ears has the user even with the odd ones

---
+++ week-03/day-4/Exercise6_BunniesAgain/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise6_BunniesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give us the number of your Bunnies >>>");
+            int bunnyNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result is: " + BunnyEarCounter2(bunnyNumber));
+            Console.ReadLine();
+        }
+        public static int BunnyEarCounter2(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            if (number % 2 == 0)
+            {
+                return 3 + BunnyEarCounter2(number - 1);
+            }
+            else
+            {
+            return 2 + BunnyEarCounter2(number - 1);
+            }
+        }
+    }
+}
