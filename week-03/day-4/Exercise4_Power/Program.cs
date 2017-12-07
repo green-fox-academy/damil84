9098f7639c4acbce54e4f747e24b9732c5ed7e70
damil84 <damil84@gmail.com>
2017-12-07 14:55:51 +0100

    (fear)Exercise4_Power is ready with user input numbers and the possibility to do another calculation while he/she wants

--- week-03/day-4/Exercise4_Power/Program.cs
+++ week-03/day-4/Exercise4_Power/Program.cs
@@ -6,20 +6,42 @@ namespace Exercise4_Power
     {
         static void Main(string[] args)
         {
-            double result = Pow(2, 10);
-            Console.WriteLine(result); // 1024
-            Console.ReadKey();
+            
+            bool yesOrNo = true;
+            do
+            {
+                Console.WriteLine("Please give the base number of your choosing >>>");
+                double inputBase = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Now Please give the power of your choosing >>>");
+
+                int inputPower = int.Parse(Console.ReadLine());
+                double result = Power(inputBase, inputPower);
+
+                Console.WriteLine("The result is: " + result);
+                Console.WriteLine();
+                Console.WriteLine();
+
+
+                Console.WriteLine("Would you like to do another calculation? y/n");
+
+                if (Console.ReadKey(true).KeyChar == 'n')
+                    { yesOrNo = false;
+                    break; }
+            }
+            while (yesOrNo);
+
         }
 
-        static public double Pow(double x, int y)
+        static public double Power(double inputBase, int inputPower)
         {
-            if (y == 0)
+            if (inputPower == 0)
             {
                 return 1.0;
             }
             else
             {
-                return x * Pow(x, y - 1);
+                return inputBase * Power(inputBase, inputPower - 1);
 
             } 
         }
