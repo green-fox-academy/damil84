fe905fc4766990ac7b64a2b8c4413a6646750d16
damil84 <damil84@gmail.com>
2017-12-07 13:54:13 +0100

    Exercise4_Power folder added

---
+++ week-03/day-4/Exercise4_Power/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise4_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = Pow(2, 10);
+            Console.WriteLine(result); // 1024
+            Console.ReadKey();
+        }
+
+        static public double Pow(double x, int y)
+        {
+            if (y == 0)
+            {
+                return 1.0;
+            }
+            else
+            {
+                return x * Pow(x, y - 1);
+
+            } 
+        }
+    }
+}
