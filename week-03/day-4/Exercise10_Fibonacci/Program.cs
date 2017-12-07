4ead86ce91f454c187347b2af121f5b4c2dde57d
damil84 <damil84@gmail.com>
2017-12-07 16:36:53 +0100

    (fear)Exercise10_Fibonacci we can calculate the nth fibonacci number given by the user

--- week-03/day-4/Exercise10_Fibonacci/Program.cs
+++ week-03/day-4/Exercise10_Fibonacci/Program.cs
@@ -7,20 +7,26 @@ namespace Exercise10_Fibonacci
         static void Main(string[] args)
         {
             Console.WriteLine("Please give us the number you'd like to fibonaccied>>>");
-            int ibputNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("The result is: " + FibonacciNumber(ibputNumber));
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The {0}nth fibonacci number is: {1}", inputNumber, FibonacciNumber(inputNumber));
             Console.ReadLine();
+            
         }
+
         public static int FibonacciNumber(int number)
         {
             if (number == 0)
             {
                 return 0;
             }
-            else
-                Console.WriteLine(number);
+
+            else if (number == 1) 
             {
-                return 2 + FibonacciNumber(number - 1);
+                return 1;
+            }
+            else
+            {
+                return FibonacciNumber(number - 1) + FibonacciNumber(number - 2);
             }
         }
     }
