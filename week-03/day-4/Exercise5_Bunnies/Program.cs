4f9df5bb4c8e85cbc879d3703bd9ae60a2eafed6
damil84 <damil84@gmail.com>
2017-12-07 15:18:02 +0100

    (fear)Exercise5_Bunnies From user input we could count how many total bunny ears has the user

---
+++ week-03/day-4/Exercise5_Bunnies/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise5_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give us the number of your Bunnies >>>");
+            int bunnyNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result is: " + BunnyEarCounter(bunnyNumber));
+            Console.ReadLine();
+        }
+        public static int BunnyEarCounter(int number)
+        {
+            if (number == 0)
+            {
+                return 0;
+            }
+            else
+                Console.WriteLine(number);
+            {
+                return 2 + BunnyEarCounter(number - 1);
+            }
+        }
+    }
+}
