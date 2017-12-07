1e9690ce216dad0aa522453e1c223614cd4ea312
damil84 <damil84@gmail.com>
2017-12-07 11:41:44 +0100

    (Feat) week-03/day-4 Exercise2_NumberAdder, Recursive adder that adds numbers from 1 to user's input number

---
+++ week-03/day-4/Exercise2_NUmberAdder/Exercise2_NUmberAdder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise2_NUmberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give us the number of your choosing >>>");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result is: " + NumberAdder(inputNumber));
+            Console.ReadLine();
+        }
+        public static int NumberAdder(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number + NumberAdder(number - 1);
+            }
+        }
+    }
+}
+
