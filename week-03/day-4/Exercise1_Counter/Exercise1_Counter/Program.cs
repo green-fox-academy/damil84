c2f5493e5f965ef16ffcfe92321c9612a45aa9c0
damil84 <damil84@gmail.com>
2017-12-07 10:57:27 +0100

    (Feat) week-03/day-4 Exercise1_Counter, Recursive counter count down from 15000 to 1

---
+++ week-03/day-4/Exercise1_Counter/Exercise1_Counter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise1_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter(15000);
+            Console.ReadLine();
+        }
+
+        public static int Counter(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                Console.WriteLine(n);
+                return Counter(n - 1);
+            }
+        }
+    }
+
+}
