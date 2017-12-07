e17b6c6b75f8cf8fb184454b33acaf5417694300
damil84 <damil84@gmail.com>
2017-12-07 12:19:13 +0100

    (Feat) week-03/day-4 Exercise3_SumDigit, Summing the digits of user's input number

---
+++ week-03/day-4/Exercise3_SUmDigit/Exercise3_SUmDigit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise3_SUmDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give us the number of your choosing >>>");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The sum of the digits of your number is: " + SumDigit(inputNumber));
+            Console.ReadLine();
+        }
+
+        public static int SumDigit(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            else
+            {
+                return (n % 10) + SumDigit(n / 10);
+            }
+        }
+    }
+}
