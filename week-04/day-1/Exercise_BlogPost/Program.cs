20898b559b7ba16a189c49f9a59d97d9e134875b
damil84 <damil84@gmail.com>
2017-12-11 15:23:21 +0100

    week-04/day-1 Exercise_BlogPost has been created.

--- week-04/day-1/Exercise_BlogPost/Program.cs
+++ week-04/day-1/Exercise_BlogPost/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Exercise_BlogPost
+namespace Exercise_PostitStruct
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var blog = new Postit();
+            blog.AuthorName = "John Doe";
+            blog.Title = "Lorem ipsum";
+            blog.Text = "Lorem ipsum dolor sit amet.";
+            blog.PublicationDate = "2000.05.04.";
+
+            var blog1 = new Postit();
+            blog1.AuthorName = "Tim Urban";
+            blog1.Title = "Wait but why";
+            blog1.Text = "A popular long-form, stick-figure-illustrated blog about almost everything";
+            blog1.PublicationDate = "2010.10.10.";
+
+            var blog2 = new Postit();
+            blog2.AuthorName = "William Turton ";
+            blog2.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            blog2.Text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center " +
+                "of attention. When I asked to take his picture outside one of IBM’s New York City offices," +
+                " he told me that he wasn’t really into the whole organizer profile thing.";
+            blog2.PublicationDate = " 2017.03.28.";
         }
     }
+
 }
