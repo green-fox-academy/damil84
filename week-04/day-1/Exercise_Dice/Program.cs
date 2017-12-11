42aaab1e35818800a87c41ebc409e9bdeda3366c
damil84 <damil84@gmail.com>
2017-12-11 20:25:31 +0100

    (feat)week-04/day-1 Exercise_Dice is ready.

--- week-04/day-1/Exercise_Dice/Program.cs
+++ week-04/day-1/Exercise_Dice/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Exercise_Dice
+namespace ConsoleApp10
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            dice Dice1 = new dice();
+
+            List<dice> dices = new List<dice>();
+            for (int i = 0; i < 6; i++)
+            {
+                dices.Add(new dice() { diceValue = 6 });
+            }
+
+            for (int j = 0; j < dices.Count; j++)
+            {
+                dices[j].Roll();
+
+                while (dices[j].GetCurrentNumber() != 6)
+                {
+                    dices[j].ReRoll();
+                }
+                Console.WriteLine(dices[j].GetCurrentNumber());
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ week-04/day-1/Exercise_Dice/dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class dice
+    {
+        public static Random random = new Random();
+        public int diceValue { get; set; }
+
+        public int GetCurrentNumber()
+        {
+            return diceValue;
+        }
+                
+        public void Roll()
+        { 
+            
+            diceValue = random.Next(1, 7);
+        }
+
+        public void ReRoll()
+        {
+            diceValue = random.Next(1, 7);
+        }
+    }
+
+}
