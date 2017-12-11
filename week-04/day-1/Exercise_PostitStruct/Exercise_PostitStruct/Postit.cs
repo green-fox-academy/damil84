d17835b86de498c1dad9da7b4175c7df738fd1be
damil84 <damil84@gmail.com>
2017-12-11 14:04:26 +0100

    (feat) week-04/day-1 Exercise_PostitStruct is ready.

---
+++ week-04/day-1/Exercise_PostitStruct/Exercise_PostitStruct/Postit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_PostitStruct
+{
+    public struct Postit
+    {
+        public string BackgrounColor;
+        public string Text;
+        public string TextColor;
+    }
+}
---
+++ week-04/day-1/Exercise_PostitStruct/Exercise_PostitStruct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_PostitStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var postit = new Postit();
+            var postit1 = new Postit();
+            var postit2 = new Postit();
+            postit.BackgrounColor = "orange";
+            postit.Text = "idea1";
+            postit.TextColor = "Blue";
+
+            postit1.BackgrounColor = "pink";
+            postit1.Text = "Awsome";
+            postit1.TextColor = "Black";
+
+            postit2.BackgrounColor = "yellow";
+            postit2.Text = "super";
+            postit2.TextColor = "green";
+
+        }
+    }
+}
