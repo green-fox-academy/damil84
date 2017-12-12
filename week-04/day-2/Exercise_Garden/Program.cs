c279e2e0902cf9ea741ac1f5d092a3ae8e8dafed
damil84 <damil84@gmail.com>
2017-12-12 18:23:21 +0100

    week-04/day-2 Exercise_Garden has been created

---
+++ week-04/day-2/Exercise_Garden/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
