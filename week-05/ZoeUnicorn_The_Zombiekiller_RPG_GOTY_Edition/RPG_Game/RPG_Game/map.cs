35b5e93da20be5eb81d519f2afa0d00be00d9a0a
damil84 <damil84@gmail.com>
2017-12-20 10:52:21 +0100

    (feat) added random generated floors to map.cs in week-05/ZoeUnicorn_The_Zombiekiller_RPG_GOTY_Edition/RPG_Game.

--- week-05/ZoeUnicorn_The_Zombiekiller_RPG_GOTY_Edition/RPG_Game/RPG_Game/map.cs
+++ week-05/ZoeUnicorn_The_Zombiekiller_RPG_GOTY_Edition/RPG_Game/RPG_Game/map.cs
@@ -9,6 +9,7 @@ namespace RPG_Game
 {
     class map
     {
+        public static Random randomNumberGenerator = new Random();
         public int MapWidth { get; private set; }
         public int MapHeigth { get; private set; }
         public FoxDraw myfoxDraw { get; set; }
@@ -20,5 +21,102 @@ namespace RPG_Game
             MapHeigth = 10;
             myfoxDraw = foxDraw;
         }
+
+        public void MapCreater()
+        {
+            tilelist = new List<int> { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 1, 0,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+                                           0, 1, 0, 1 ,0, 1, 0, 1, 1, 0,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+                                           0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+                                           0, 0, 0, 1, 0, 0, 0, 1, 1, 0,};
+            int ycoordinate = 0;
+            int listcounter = 0;
+            for (int i = 0; i < MapHeigth; i++)
+            {
+                int xcoordinate = 0;
+
+                for (int j = 0; j < MapWidth; j++)
+                {
+                    if (tilelist[listcounter] == 0)
+                    {
+                        int randomNumber = randomNumberGenerator.Next(1, 11);
+
+                        if (randomNumber == 1)
+                        {
+                            myfoxDraw.AddImage("Assets/floor1.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 2)
+                        {
+                            myfoxDraw.AddImage("Assets/floor2.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 3)
+                        {
+                            myfoxDraw.AddImage("Assets/floor3.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 4)
+                        {
+                            myfoxDraw.AddImage("Assets/floor4.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 5)
+                        {
+                            myfoxDraw.AddImage("Assets/floor5.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 6)
+                        {
+                            myfoxDraw.AddImage("Assets/floor6.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 7)
+                        {
+                            myfoxDraw.AddImage("Assets/floor7.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 8)
+                        {
+                            myfoxDraw.AddImage("Assets/floor8.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 9)
+                        {
+                            myfoxDraw.AddImage("Assets/floor9.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                        else if (randomNumber == 10)
+                        {
+                            myfoxDraw.AddImage("Assets/floor10.png", xcoordinate, ycoordinate);
+                            xcoordinate = xcoordinate + 50;
+                        }
+
+                    }
+                    if (tilelist[listcounter] == 1)
+                    {
+                        myfoxDraw.AddImage("Assets/wall2.png", xcoordinate, ycoordinate);
+                        xcoordinate = xcoordinate + 50;
+                    }
+                    listcounter++;
+                }
+                ycoordinate = ycoordinate + 50;
+            }
+        }
+
     }
 }
