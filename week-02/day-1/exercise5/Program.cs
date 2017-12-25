5dddb266a6aad298ddfea2b89e7232d171ad33a9
damil84 <damil84@gmail.com>
2017-12-25 23:35:14 +0100

    (feat) added TwoNumbers user input in week-02/day-1/exercise4

--- week-02/day-1/exercise5/Program.cs
+++ week-02/day-1/exercise5/Program.cs
@@ -6,17 +6,16 @@ namespace exercise5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            int number0 = 22;
-            int number1 = 13;
-            Console.WriteLine(number0 + number1);
-            Console.WriteLine(number1 - number0);
-            Console.WriteLine(number1 * number0);
-            Console.WriteLine(number0 / number1);
-            Console.WriteLine(number0 / (double)number1);
-            Console.WriteLine(number0 / number1);
-            Console.WriteLine(number0 % number1);
+            Console.WriteLine("Please type the first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please now type the second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result of adding is: " + (firstNumber + secondNumber));
+            Console.WriteLine("The result of substraction is: " + (secondNumber - firstNumber));
+            Console.WriteLine("The result of multiplication is: " + (secondNumber * firstNumber));
+            Console.WriteLine("The result of dividation as a decimal fraction is: " + (firstNumber / (decimal)secondNumber));
+            Console.WriteLine("The result of dividation integer part is: " + (firstNumber / secondNumber));
+            Console.WriteLine("The result of dividation reminder is: " + (firstNumber % secondNumber));
 
             Console.ReadLine();
         }
