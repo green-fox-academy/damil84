541f43144eb60733670d9309598303882e5933fe
damil84 <damil84@gmail.com>
2017-12-26 00:47:01 +0100

    (refract) cleaner main with added class/function for TwoNumbers in week-02/day-1/exercise4

---
+++ week-02/day-1/exercise5/Functions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise5
+{
+    class Functions : Program
+    {
+        public void Calculations()
+        {
+            Console.WriteLine("Please type the first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please now type the second number");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("The result of adding is: " + (firstNumber + secondNumber));
+            Console.WriteLine("The result of substraction is: " + (secondNumber - firstNumber));
+            Console.WriteLine("The result of multiplication is: " + (secondNumber * firstNumber));
+            Console.WriteLine("The result of dividation as a decimal fraction is: " + (firstNumber / (decimal)secondNumber));
+            Console.WriteLine("The result of dividation integer part is: " + (firstNumber / secondNumber));
+            Console.WriteLine("The result of dividation reminder is: " + (firstNumber % secondNumber));
+
+        }
+    }
+}
--- week-02/day-1/exercise5/Program.cs
+++ week-02/day-1/exercise5/Program.cs
@@ -6,17 +6,8 @@ namespace exercise5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please type the first number");
-            int firstNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please now type the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("The result of adding is: " + (firstNumber + secondNumber));
-            Console.WriteLine("The result of substraction is: " + (secondNumber - firstNumber));
-            Console.WriteLine("The result of multiplication is: " + (secondNumber * firstNumber));
-            Console.WriteLine("The result of dividation as a decimal fraction is: " + (firstNumber / (decimal)secondNumber));
-            Console.WriteLine("The result of dividation integer part is: " + (firstNumber / secondNumber));
-            Console.WriteLine("The result of dividation reminder is: " + (firstNumber % secondNumber));
-
+            Functions calculations = new Functions();
+            calculations.Calculations();
             Console.ReadLine();
         }
     }
