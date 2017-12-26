17bc2a0fd351901f3b5f3c6a92257c95dfa3d8b2
damil84 <damil84@gmail.com>
2017-12-26 14:01:50 +0100

    (feat) try,parse input validation for user input in 'TwoNumbers' week-02/day-1/exercise5

--- week-02/day-1/exercise5/Functions.cs
+++ week-02/day-1/exercise5/Functions.cs
@@ -7,16 +7,26 @@ namespace exercise5
         public void Calculations()
         {
             Console.WriteLine("Please type the first number");
-            int firstNumber = int.Parse(Console.ReadLine());
+            int firstNumber;
+            while (!int.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.WriteLine("This is not an integer number, please try to type first number again");
+            }
+
             Console.WriteLine("Please now type the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("The result of adding is: " + (firstNumber + secondNumber));
+            int secondNumber;
+            while (!int.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.WriteLine("This is not an integer number, please try to type second number again");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The result of adding is: " + (secondNumber + firstNumber));
             Console.WriteLine("The result of substraction is: " + (secondNumber - firstNumber));
-            Console.WriteLine("The result of multiplication is: " + (secondNumber * firstNumber));
+            Console.WriteLine("The result of multiplication is: " + (firstNumber * secondNumber));
             Console.WriteLine("The result of dividation as a decimal fraction is: " + (firstNumber / (decimal)secondNumber));
             Console.WriteLine("The result of dividation integer part is: " + (firstNumber / secondNumber));
             Console.WriteLine("The result of dividation reminder is: " + (firstNumber % secondNumber));
-
         }
     }
 }
