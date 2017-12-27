74f82641b8399477317bf98e9acaf5869fe1c3fa
damil84 <damil84@gmail.com>
2017-12-27 23:51:35 +0100

    redo 'BMI' in week-02/day-1/exercise9 with clean main and class/method way for pratice

---
+++ week-02/day-1/exercise9/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercise9
+{
+    class Functions
+    {
+        public void BMIcalculation()
+        {
+            Console.WriteLine("Please type in your mass in kilograms >>");
+            double massInKg;
+            while (!double.TryParse(Console.ReadLine(), out massInKg))
+            {
+                Console.WriteLine("This is not a number, or you used '.' insted of ',' for fraction. Please try to type in numbers again >>");
+            }
+
+            Console.WriteLine("Please type in your height in meter >>");
+            double heightInM;
+            while (!double.TryParse(Console.ReadLine(), out heightInM))
+            {
+                Console.WriteLine("This is not a number, or you used '.' insted of ',' for fraction. Please try to type in numbers again >>");
+            }
+
+            double bmi = massInKg / (heightInM * heightInM);
+            Console.WriteLine("Your BMI is " + bmi);
+        }
+
+    }
+}
--- week-02/day-1/exercise9/Program.cs
+++ week-02/day-1/exercise9/Program.cs
@@ -6,12 +6,8 @@ namespace exercise9
     {
         static void Main(string[] args)
         {
-            // Print the Body mass index (BMI) based on these values
-            double massInKg = 81.2;
-            double heightInM = 1.78;
-
-            double bmi = massInKg / (heightInM*heightInM);
-            Console.WriteLine("A BMI is " + bmi);
+            Functions bmi = new Functions();
+            bmi.BMIcalculation();           
             Console.ReadLine();
         }
     }
