32925a3af2a82e8912e67c33c2ee95755ece5824
damil84 <damil84@gmail.com>
2017-12-28 01:28:57 +0100

    (feat) added written result in 'BMI', week-02/day-1/exercise9

--- week-02/day-1/exercise9/Functions.cs
+++ week-02/day-1/exercise9/Functions.cs
@@ -27,7 +27,30 @@ namespace exercise9
 
             double bmi = massInKg / (heightInM * heightInM);
             Console.WriteLine("Dear {0}, your BMI value is {1} ", name, bmi);
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("According to your BMI value, you are underweight");
+            }
+            else if (bmi > 18.5 && bmi < 24.9 )
+            {
+                Console.WriteLine("According to your BMI value, you are in normal weight");
+            }
+            else if (bmi > 25 && bmi < 29.9)
+            {
+                Console.WriteLine("According to your BMI value, you are overweight");
+            }
+            else if (bmi > 30 && bmi < 34.9)
+            {
+                Console.WriteLine("According to your BMI value, you are in obesity calss I");
+            }
+            else if (bmi > 35 && bmi < 39.9)
+            {
+                Console.WriteLine("According to your BMI value, you are in obesity calss II");
+            }
+            else if (bmi >= 40)
+            {
+                Console.WriteLine("According to your BMI value, you are in obesity calss I");
+            }
         }
-
     }
 }
