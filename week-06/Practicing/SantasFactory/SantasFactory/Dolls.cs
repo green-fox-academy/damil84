b0200da6fd4048986594d9cb36f05e06d6fe800b
damil84 <damil84@gmail.com>
2018-01-06 23:04:31 +0100

    also added Dolls class, SantasFactory exercise in week-06/Practice folder

---
+++ week-06/Practicing/SantasFactory/SantasFactory/Dolls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SantasFactory
+{
+    class Dolls : Toys
+    {
+        public string Color { get; set; }
+
+        public Dolls(string type, string color) : base (type, 25)
+        {
+            Color = color;
+        }
+    }
+}
