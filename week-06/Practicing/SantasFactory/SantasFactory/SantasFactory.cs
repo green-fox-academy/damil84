f0bfb93118575f40250ed19d78a9e8542104ae71
damil84 <damil84@gmail.com>
2018-01-07 14:16:23 +0100

    (fixed) produce methodes in SantasFactory class, SantasFactory exercise in week-06/Practice folder

--- week-06/Practicing/SantasFactory/SantasFactory/SantasFactory.cs
+++ week-06/Practicing/SantasFactory/SantasFactory/SantasFactory.cs
@@ -14,16 +14,19 @@ namespace SantasFactory
         public Toys Produce(string type, string color)
         {
             Toys doll = new Dolls(type, color);
+            return doll;
         }
 
         internal Toys Produce(string type, string color, int size)
         {
             Toys ball= new DottedBalls(type, color, size);
+            return ball;
         }
 
         public Toys Produce(string type, int lenght)
         {
             Toys rope = new JumpingRopes(type, lenght);
+            return rope;
         }
     }
 }
