417774fcd26c3bc7d659270e4b4d2ea038c57a91
damil84 <damil84@gmail.com>
2018-01-16 17:06:24 +0100

    some more practice through homework videos and moving folders to their correct place, AspNetPractice and AspNetPractice_3 exercises in week-07 folder

---
+++ week-07/Practice/AspNetPratice_3/AspNetPratice_3/Controllers/WhatController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetPratice_3.Controllers
+{
+    public class WhatController : Controller
+    {
+        // GET: What
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: What/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: What/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: What/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: What/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: What/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: What/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: What/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
