ce0cb202faf8864460eeca58b67506ab5eaae875
damil84 <damil84@gmail.com>
2018-01-17 11:07:47 +0100

    (feat) 5 folders and projects-base from week-07/day-1 exercises

---
+++ week-07/day-1/WebGreetCounter/WebGreetCounter/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebGreetCounter.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("")]
+        [Route("hello")]
+        public IActionResult Hello()
+        {
+            return Content("Hello-bello, dear world!");
+        }
+
+
+
+
+    }
+}
