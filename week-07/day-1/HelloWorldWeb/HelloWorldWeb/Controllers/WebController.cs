370841a496935d7e70e9962cce2277cccf955225
damil84 <damil84@gmail.com>
2018-01-25 10:33:57 +0100

    (feat) HelloWorldWb exercise is ready in week-07/day-1 folder

---
+++ week-07/day-1/HelloWorldWeb/HelloWorldWeb/Controllers/WebController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HelloWorldWeb.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloWorldWeb.Controllers
+{
+    [Route("")]
+    [Route("web")]
+    public class WebController : Controller
+    {
+        [Route("Greeting")]
+        public IActionResult Greeting()
+        {
+            var greeting = new Greeting()
+            {
+                Id = 1,
+                Content = "World"
+            };
+
+            return View(greeting);
+        }
+    }
+}
---
+++ week-07/day-1/HelloWorldWeb/HelloWorldWeb/Models/Greeting.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorldWeb.Models
+{
+    public class Greeting
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+
+        public Greeting(string userName, int userCounter)
+        {
+            Id = userCounter;
+            Content = "Hello-bello-jello, " + userName;
+        }
+
+        public Greeting()
+        {
+        }
+    }
+}
