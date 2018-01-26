e96c27cecba1fd7a64d3219bd15b2f9a92af6925
damil84 <damil84@gmail.com>
2018-01-26 16:42:13 +0100

    (init) subfolders created for BankOfSimba week-07/day-1 folder

--- week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Startup.cs
+++ week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Startup.cs
@@ -21,6 +21,9 @@ namespace BankOfSimba
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,7 +33,6 @@ namespace BankOfSimba
             {
                 await context.Response.WriteAsync("Hello World!");
             });
-            app.UseMvc();
 
         }
     }
