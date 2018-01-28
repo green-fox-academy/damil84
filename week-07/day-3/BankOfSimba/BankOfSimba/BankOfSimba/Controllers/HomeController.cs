7e02bf891cfde8d97ada2c60fa97241188a1704f
damil84 <damil84@gmail.com>
2018-01-28 17:14:58 +0100

    (feat)Bank of Simba Part I. is ready in week-07/day-1 folder

--- week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
+++ week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Controllers/HomeController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using BankOfSimba.Models;
 
 namespace BankOfSimba.Controllers
-{
+{   
+    [Route("")]
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        //// GET: /<controller>/
+        [Route("index")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        //[Route("simba")]
+        [HttpGet("Simba")]
+        public IActionResult Simba()
+        {
+            BankAccount account = new BankAccount("Simba", 2000, "Lion");
+
+            return View(account);
         }
     }
 }
--- week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Models/BankAccount.cs
+++ week-07/day-3/BankOfSimba/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -7,5 +7,15 @@ namespace BankOfSimba.Models
 {
     public class BankAccount
     {
+        public string Name { get; set; }
+        public int Balance { get; set; }
+        public string AnimalType { get; set; }
+
+        public BankAccount(string name, int balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
     }
 }
