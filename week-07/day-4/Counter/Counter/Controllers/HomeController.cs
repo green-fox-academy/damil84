e9f80fa6f0ceae51a24bfde8cd23d90001a712ac
damil84 <damil84@gmail.com>
2018-01-30 16:45:42 +0100

    feat(Counter) Counter exercise is ready in week-07/day-4 folder

---
+++ week-07/day-4/Counter/Counter/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Counter.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Counter.Controllers
+{   
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private CounterModel counter;
+
+        public HomeController(CounterModel counter)
+        {
+            this.counter = counter;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View(counter);
+        }
+
+        [HttpPost("")]
+        public IActionResult PostMethod()
+        {
+            this.counter.counter++;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ week-07/day-4/Counter/Counter/Models/CounterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Counter.Models
+{
+    public class CounterModel
+    {
+        public int counter;
+
+        public CounterModel()
+        {
+            counter = 0;
+        }
+    }
+}
