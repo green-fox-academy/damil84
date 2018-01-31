3d48a128b7e57c7ae9b6ef75d46ff5d33a54f2c0
damil84 <damil84@gmail.com>
2018-01-31 16:31:50 +0100

    feat(Exercise4) Exercise4 is ready in week-06/day-3 folder

---
+++ week-06/day-3/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            Console.WriteLine();
+            int[] numbers = { 3, 9, 2, 8, 6, 5 };
+
+            var ex4Numbers = from n in numbers
+                              where n*n > 20
+                              orderby n
+                              select n;
+
+            var ex4Numbers2 = numbers.Where(n => (n*n > 20)).OrderBy(n => n);
+
+            Console.WriteLine("Exercise1 solution with query syntax:");
+            Console.WriteLine(string.Join(", ", ex4Numbers));
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise1 solution with method syntax:");
+            Console.WriteLine(string.Join(", ", ex4Numbers2));
+            Console.ReadLine();
+        }
+    }
+}
