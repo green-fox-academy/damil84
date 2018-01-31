d4ba00a9be9440555cfd4339881bf7ea4a4816e4
damil84 <damil84@gmail.com>
2018-01-31 16:57:05 +0100

    feat(Exercise5) Exercise5 is ready in week-06/day-3 folder

---
+++ week-06/day-3/Exercise5/Exercise5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            Console.WriteLine();
+            int[] numbers = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var ex5Numbers = from n in numbers
+                             orderby n
+                             group n by n into grp
+                             select new { grp.Key, Count = grp.Count() };
+
+            var ex5Numbers2 = numbers.OrderBy(n => n).GroupBy(n => n).ToDictionary(n => n.Key, n => n.Count());
+
+            Console.WriteLine("Exercise1 solution with query syntax:");
+            Console.WriteLine(string.Join(", ", ex5Numbers));
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise1 solution with method syntax:");
+            Console.WriteLine(string.Join(", ", ex5Numbers2));
+            Console.ReadLine();
+        }
+    }
+}
