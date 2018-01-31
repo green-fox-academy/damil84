1054a6a7a932c61d035879c279851dc53d336363
damil84 <damil84@gmail.com>
2018-01-31 16:03:55 +0100

    feat(Exercise2) Exercise2 is ready in week-06/day-3 folder

---
+++ week-06/day-3/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            Console.WriteLine();
+            int[] numbers = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var evenNumbers = from n in numbers
+                              where n % 2 != 0
+                              orderby n
+                              select n;
+
+            var evenNumbers2 = numbers.Where(n => (n % 2 != 0)).OrderBy(n => n);
+
+            Console.WriteLine("Exercise1 solution with query syntax:");
+            Console.WriteLine(string.Join(", ", evenNumbers));
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise1 solution with method syntax:");
+            Console.WriteLine(string.Join(", ", evenNumbers2));
+            Console.ReadLine();
+        }
+    }
+}
