a3cf4f9b39deba28675dd082e588819806464918
damil84 <damil84@gmail.com>
2018-02-01 17:33:06 +0100

    init(PallidaOrientationExamReTake)needed folders created + model class filled with needed properties in week-10/Practice4Exam folder

---
+++ week-10/Practice4Exam/PallidaOrientationExamReTake/PallidaOrientationExamReTake/Models/WebStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaOrientationExamReTake.Models
+{
+    public class WebStore
+    {
+        public long? Id { get; set; }
+        public string ItemName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Category { get; set; }
+        public string Size { get; set; }
+        public float UnitPrice { get; set; }
+    }
+}
