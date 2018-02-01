81701a76628a6995c6c33649f47c2eb402710557
damil84 <damil84@gmail.com>
2018-02-01 10:35:55 +0100

    init(PallidaOrientationExam) in week-10/Practice folder

---
+++ week-06/day-3/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS", "ACADEMI" }; Console.WriteLine();
+
+            var stringFreq = from n in cities
+                             orderby n
+                             group n by n into grp
+                             select new { grp.Key, Count = grp.Count() };
+
+            var stringFreq2 = cities.OrderBy(n => n).GroupBy(n => n).ToDictionary(n => n.Key, n => n.Count());
+
+            Console.WriteLine("Exercise solution with query syntax:");
+            Console.WriteLine(string.Join(", ", stringFreq));
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise solution with method syntax:");
+            Console.WriteLine(string.Join(", ", stringFreq2));
+            Console.ReadLine();
+        }
+    }
+}
