c0e58e151d137f2d6a3cb8025092964e74b5f218
damil84 <damil84@gmail.com>
2018-02-01 10:49:22 +0100

    init(PallidaOrientationExam)enitites and model class added and filled in week-10/Practice4Exam folder

---
+++ week-10/Practice4Exam/PallidaExam/PallidaExam/Entities/CarsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PallidaExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaExam.Entities
+{
+    public class CarsContext : DbContext
+    {
+        public CarsContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Cars> Cars { get; set; }
+    }
+}
