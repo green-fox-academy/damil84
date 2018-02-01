a64395c22a9fa18f46d922281d6309ea7783d56a
damil84 <damil84@gmail.com>
2018-02-01 19:35:05 +0100

    init(PallidaOrientationExamReTake) added and filled entitites context class in week-10/Practice4Exam folder

---
+++ week-10/Practice4Exam/PallidaOrientationExamReTake/PallidaOrientationExamReTake/Entities/WebStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PallidaOrientationExamReTake.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PallidaOrientationExamReTake.Entities
+{
+    public class WebStoreContext : DbContext
+    {
+        public WebStoreContext(DbContextOptions options) : base (options)
+        {
+
+        }
+        public DbSet<WebStore> WebStore { get; set; }
+    }
+}
