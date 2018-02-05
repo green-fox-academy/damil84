1eaae31f5ad232a0f54c7f0ee19e53a68494173c
damil84 <damil84@gmail.com>
2018-02-05 23:28:36 +0100

    feat(exercise9) exercise9 is ready in week-06/day-3

---
+++ week-06/day-3/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            Console.WriteLine("Type your text here, to creat a character array!");
+            var userString = Console.ReadLine().ToCharArray();
+            Console.WriteLine();
+
+            var stringConvert1 = from c in userString
+                               select c.ToString();
+            string text1 = string.Join(string.Empty, stringConvert1);
+
+
+            var stringConvert2 = userString.Select(c => char.ToString(c));
+            string text2 = string.Join(string.Empty, stringConvert2);
+            Console.WriteLine("Exercise solution with query syntax:");
+            Console.WriteLine(text1);
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise solution with method syntax:");
+            Console.WriteLine(text2);
+            Console.ReadLine();
+        }
+    }
+}
