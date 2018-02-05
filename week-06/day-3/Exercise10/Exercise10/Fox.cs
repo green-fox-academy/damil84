40b98af524626227435b31ca68c29abb1dbab4da
damil84 <damil84@gmail.com>
2018-02-06 00:01:47 +0100

    feat(exercise10) exercise10 is ready in week-06/day-3

---
+++ week-06/day-3/Exercise10/Exercise10/Fox.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise10
+{
+    class Fox
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Fox(string name, string type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
+    }
+}
---
+++ week-06/day-3/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxList = new List<Fox>()
+            {
+                new Fox("Don Kartács", "asbest", "green"),
+                new Fox("kisvuk", "badcat", "red"),
+                new Fox("Kara", "please", "red"),
+                new Fox("Foxi Maxi", "asbest", "green"),
+                new Fox("Red Skull", "asbest", "red"),
+                new Fox("Elizabeth", "please", "yellow"),
+                new Fox("Róka Rudi", "asbest", "green"),
+                new Fox("Naruto", "please", "green"),
+                new Fox("Foxy Lady", "badcat", "green")
+            };
+
+            Console.WriteLine("Here are the greem foxes names:");
+            foxList.Where(fox => fox.Color == "green").ToList().ForEach(fox => Console.WriteLine(fox.Name));
+
+            Console.WriteLine();
+            Console.WriteLine("And here are the green foxes of the asbest corsac:");
+
+            foxList.Where(fox => fox.Color == "green" && fox.Type == "asbest").ToList().ForEach(fox => Console.WriteLine(fox.Name));
+
+            Console.ReadLine();
+        }
+    }
+}
