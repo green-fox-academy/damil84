91650e04310ea8bf05d5ec2969ab52ab5e0dbb15
damil84 <damil84@gmail.com>
2018-02-05 20:27:28 +0100

    feat(exercise8) exercise8 is ready in week-06/day-3

---
+++ week-06/day-3/Exercise8/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dear User!");
+            Console.WriteLine("Type your text here!");
+            string userString = Console.ReadLine();
+            char[] ary = userString.ToCharArray();
+            Console.WriteLine();
+
+            var stringUpper1 = from a in userString
+                               where a.ToString() == a.ToString().ToUpper()
+                               select a;
+
+            var stringUpper2 = userString.Where(c => char.IsUpper(c));
+
+            Console.WriteLine("Exercise1 solution with query syntax:");
+            Console.WriteLine(string.Join(", ", stringUpper1));
+            Console.WriteLine();
+
+            Console.WriteLine("Exercise1 solution with method syntax:");
+            Console.WriteLine(string.Join(", ", stringUpper2));
+            Console.ReadLine();
+        }
+    }
+}
