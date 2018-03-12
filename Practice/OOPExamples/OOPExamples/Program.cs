68afd8b72e13d8e1e42582513032133efc0feb26
damil84 <damil84@gmail.com>
2018-03-12 18:56:38 +0100

    feat(OOPExamples) comparing a variable to type of class with a struct boolean

--- Practice/OOPExamples/OOPExamples/Program.cs
+++ Practice/OOPExamples/OOPExamples/Program.cs
@@ -6,5 +6,11 @@ class Program
     static void Main(string[] args)
     {
         var comp = new Computer();
+        var isComputer = comp is Computer;
+
+        Console.WriteLine("Computer type - " + comp.GetType());
+        Console.WriteLine("Is computer - " + isComputer);
+
+        Console.ReadLine();
     }
 }
